430162131f645463cdd7a9bc336ad66307720c54
molvqingtai <molvqingtai@gmail.com>
2025-02-12 10:07:42 +0800

    ci: update workflows

--- __tests__/fixtures/iframe.ts
+++ __tests__/fixtures/iframe.ts
@@ -1,5 +1,6 @@
 import { Browser } from 'happy-dom'
 import { resolve } from 'node:path'
+import { findByTestId } from '@testing-library/dom'
 
 const browser = new Browser({
   settings: {
@@ -17,9 +18,8 @@ const browser = new Browser({
 const newPage = async () => {
   const page = browser.newPage()
   await page.goto('https://localhost:8080')
-
   const main = page.mainFrame.document as unknown as HTMLElement
-  const iframe = main.querySelector<HTMLIFrameElement>('#iframe')!.contentDocument as unknown as HTMLElement
+  const iframe = (await findByTestId<HTMLIFrameElement>(main, 'iframe'))!.contentDocument as unknown as HTMLElement
   return { page, main, iframe }
 }
 
